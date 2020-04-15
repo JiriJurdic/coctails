ba6cd8a96754af3a8b92bf22a3ef113d1ff4eb94
JiriJurdic <jiri.jurdic@gmail.com>
2020-04-15 23:34:59 +0200

    ServletInitializer

---
+++ src/main/java/com/coctail/coctail/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.coctail.coctail;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder aplication) {
+		return aplication.sources(CoctailApplication.class);
+	}
+}
