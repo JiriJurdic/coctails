33b26721a8a4da03c2bd9a7274ac192572d5a770
JiriJurdic <jiri.jurdic@gmail.com>
2020-04-15 22:56:16 +0200

    Spring Initializr

---
+++ src/test/java/com/coctail/coctail/CoctailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coctail.coctail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
